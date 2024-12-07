cc6d59be831abfaba3248770a777b7b7accb3bed
IliaKataev <i.kataev.sas@gmail.com>
2024-12-08 01:49:56 +0500

    не заметил неработающее "появляющееся" меню. Исправил

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log(document.getElementById('test-id'))
-// })
+document.addEventListener("DOMContentLoaded", () => {
+    const burgerIcon = document.getElementById("burger-icon");
+    const menuOverlay = document.getElementById("menu-overlay");
+    const closeMenu = document.getElementById("close-menu");
+
+    burgerIcon.addEventListener("click", () => {
+        menuOverlay.style.display = "flex";
+    });
+
+    closeMenu.addEventListener("click", () => {
+        menuOverlay.style.display = "none";
+    });
+
+    menuOverlay.addEventListener("click", (event) => {
+        if (event.target === menuOverlay) {
+            menuOverlay.style.display = "none";
+        }
+    });
+});
